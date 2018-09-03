ead30773612c621d2e2a710166258b6285980c84
malliswari3 <42571548+malliswari3@users.noreply.github.com>
2018-09-03 14:50:32 +0530

    Create consonant.py

---
+++ consonant.py
@@ -0,0 +1,7 @@
+list=["a","A","e","E","i","I","o","O","u","U"]
+list1=["b","c","d","f","g","h","j","k","l","m","n","p","q","r","s","t","v","w","x","y","z","B","C","D","F","G","H","J","K","L","M","N","P","Q","R","S","T","V","W","X","Y","Z"]
+s=str(input())
+if(s in list):
+  print("vowel")
+if(s in list1):
+  print("Consonant")
